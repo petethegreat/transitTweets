8b78d7b489d887d73fab2b5df7cc838d16e94f86
Peter Thompson <petethegreat@gmail.com>
2018-02-15 19:37:32 -0500

    sorting out ssl, added error handling to tweepy, narrowed location box

--- web/js/leafMap.js
+++ web/js/leafMap.js
@@ -8,7 +8,7 @@ var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a>'
 var stamenTileUrl = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png'
 var stamenTileAttrib = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
-var geoDataSourceURL = 'www.petesstuff.ml:9898/geojsonData'
+var geoDataSourceURL = 'http://www.petesstuff.ml:9898/geojsonData'
 L.tileLayer(stamenTileUrl,{
     attribution: stamenTileAttrib,
     minZoom: 0,
