dd3ae777940bb5c1330ce63665932bb7837ef3f7
Peter Thompson <petethegreat@gmail.com>
2018-03-21 14:13:13 -0400

    moose

---
+++ processTweets/processSentiment.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' processSentiment
+load raw tweet data, carry out sentiment analysis
+save results in a sentiment table
+'''
+
+# 
+
--- testProcess.py
+++ testProcess.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
-import ttweeter
+import processTweets
 
-moose = ttweeter.tweetProcessor()
+# get relevant arguments from command line, or use defaults
 
-moose.processRawTweets()
+# a recent db dump is at ~/temp/rawTweets.db
+
+# get tweets that haven't been sentimented
+# select id from rawtweets where id not in (select id from sentiments);
+
+# run tweets through sentiment analysis, nltk
+
+# get word importance in tfidf
+# maybe seperate this by east/west or positive/negative sentiment
+
+
+
+
+
+
